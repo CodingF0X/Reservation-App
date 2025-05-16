a9e8d2dc1ffd028888734e42a4338492d6cdb979
CodingF0X <roody9a@gmail.com>
2025-05-16 14:37:03 +0200

    -- configured the microservice

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PaymentsModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create(PaymentsModule, { bufferLogs: true });
+
+  const conifgService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+  app.use(cookieParser());
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: conifgService.getOrThrow('PAYMENTS_TCP_PORT'),
+      host: conifgService.getOrThrow('PAYMENTS_HOST'),
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(conifgService.getOrThrow('PAYMENTS_HTTP_PORT'));
+
+  console.log(
+    `HTTP server listening on ${conifgService.getOrThrow<number>(
+      'PAYMENTS_HTTP_PORT',
+    )}, TCP on ${conifgService.get<number>('PAYMENTS_TCP_PORT')}`,
+  );
 }
 bootstrap();
--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ConfigModule } from '@app/common';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+            autoLogging: false,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
