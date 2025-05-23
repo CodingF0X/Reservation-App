66a7764298bb03b7c61a378cb6478f60b0300e3f
CodingF0X <roody9a@gmail.com>
2025-05-23 12:28:16 +0200

    registered auth service via TCP

--- api-gateway/apps/api-gateway/src/gateway/gateway.module.ts
+++ api-gateway/apps/api-gateway/src/gateway/gateway.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { GatewayService } from './gateway.service';
 import { GatewayController } from './gateway.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, SERVICES } from '@app/common';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: SERVICES.AUTH_SERVICE,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.getOrThrow<string>('AUTH_HOST'),
+            port: configService.getOrThrow<number>('AUTH_TCP_PORT'),
+          },
+        }),
+      },
+    ]),
+  ],
   providers: [GatewayService],
-  controllers: [GatewayController]
+  controllers: [GatewayController],
 })
 export class GatewayModule {}
