616cdd1c6bb8bd425ff303a98f39785b917544ab
CodingF0X <roody9a@gmail.com>
2025-05-24 22:03:57 +0200

    updated the uriConfig to retrieve same env from a const

--- api-gateway/libs/common/src/config/uri.config.ts
+++ api-gateway/libs/common/src/config/uri.config.ts
@@ -1,10 +1,11 @@
 import { ConfigObject, registerAs } from '@nestjs/config';
 
-type Service = 'auth' | 'reservations';
-export default registerAs(
-  'services',
-  (): ConfigObject => ({
-    auth: process.env.AUTH_SERVICE,
+export default registerAs('services', (): ConfigObject => {
+  const authServiceUri = process.env.AUTH_SERVICE;
+
+  return {
+    auth: authServiceUri,
+    users: authServiceUri,
     reservations: process.env.RESERVATIONS_SERVICE,
-  }),
-);
+  };
+});
