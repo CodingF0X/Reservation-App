53428e56b90fed455e06d0be4898df3dafb482d9
CodingF0X <roody9a@gmail.com>
2025-05-26 20:08:37 +0200

    implemented the rest of CRUD ops on the property

--- draftr/apps/property-category/src/property-category.controller.ts
+++ draftr/apps/property-category/src/property-category.controller.ts
@@ -1,14 +1,42 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { PropertyCategoryService } from './property-category.service';
 import { CreatePropertyDto } from './dto/create-prop.dto';
 import { PropertyCategory } from './entities/property.entity';
+import { UpdatePropertyDto } from './dto/update-prop.dto';
 
 @Controller('properties')
 export class PropertyCategoryController {
-  constructor(private readonly propertyCategoryService: PropertyCategoryService) {}
+  constructor(
+    private readonly propertyCategoryService: PropertyCategoryService,
+  ) {}
 
   @Post()
-  async addNewProprty (@Body() body: CreatePropertyDto): Promise<PropertyCategory> {
+  async addNewProprty(
+    @Body() body: CreatePropertyDto,
+  ): Promise<PropertyCategory> {
     return this.propertyCategoryService.addNewProperty(body);
+  }
+
+  @Get()
+  async getAllProperties(): Promise<PropertyCategory[]> {
+    return this.propertyCategoryService.getAllProperties();
+  }
+
+  @Get(':id')
+  async getPropertyById(@Param('id') id: string): Promise<PropertyCategory> {
+    return this.propertyCategoryService.getPropertyById(id);
+  }
+
+  @Patch(':id')
+  async updateProperty(
+    @Param('id') id: string,
+    @Body() body: UpdatePropertyDto,
+  ): Promise<PropertyCategory> {
+    return this.propertyCategoryService.updateProperty(id, body);
+  }
+
+  @Delete(':id')
+  async deleteProperty(@Param('id') id: string): Promise<string> {
+    return this.propertyCategoryService.deleteProperty(id);
   }
 }
--- draftr/apps/property-category/src/property-category.service.ts
+++ draftr/apps/property-category/src/property-category.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { PropertiesRepository } from './properties.repository';
 import { PropertyCategory } from './entities/property.entity';
 import { CreatePropertyDto } from './dto/create-prop.dto';
+import { UpdatePropertyDto } from './dto/update-prop.dto';
 
 @Injectable()
 export class PropertyCategoryService {
@@ -9,9 +14,92 @@ export class PropertyCategoryService {
 
   constructor(private readonly propertiesRepo: PropertiesRepository) {}
 
-  async addNewProperty (body: CreatePropertyDto): Promise<PropertyCategory>{
-    const newProperty = await this.propertiesRepo.create(body)
+  async addNewProperty(body: CreatePropertyDto): Promise<PropertyCategory> {
+    try {
+      const newProperty = await this.propertiesRepo.create(body);
 
-    return newProperty;
+      return newProperty;
+    } catch (error) {
+      this.logger.error(
+        `Failed to create property for host ${body.hostId}`,
+        error.stack,
+      );
+
+      throw new InternalServerErrorException(
+        'Could not create property, please try again later.',
+        error,
+      );
+    }
+  }
+
+  async getAllProperties(): Promise<PropertyCategory[]> {
+    try {
+      const allProperties = await this.propertiesRepo.find({});
+
+      return allProperties;
+    } catch (error) {
+      this.logger.error(`Failed to get all properties`, error);
+
+      throw new InternalServerErrorException(
+        'Could not get all properties, please try again later.',
+        error,
+      );
+    }
+  }
+
+  async getPropertyById(id: string): Promise<PropertyCategory> {
+    try {
+      const property = await this.propertiesRepo.findOne({ _id: id });
+
+      return property;
+    } catch (error) {
+      this.logger.error(`Failed to get property with id ${id}`, error);
+
+      throw new InternalServerErrorException(
+        'Could not get property, please try again later.',
+        error,
+      );
+    }
+  }
+
+  async updateProperty(
+    id: string,
+    body: UpdatePropertyDto,
+  ): Promise<PropertyCategory> {
+    try {
+      const updatedProperty = await this.propertiesRepo.findOneAndUpdate(
+        { _id: id },
+        body,
+      );
+
+      return updatedProperty;
+    } catch (error) {
+      this.logger.error(`Failed to update property with id ${id}`, error);
+
+      throw new InternalServerErrorException(
+        'Could not update property, please try again later.',
+        error,
+      );
+    }
+  }
+
+  async deleteProperty(id: string): Promise<string> {
+    try {
+      // if(prop.reservations.length > 0){
+      //   throw new InternalServerErrorException(
+      //     'Could not delete property, there are reservations associated with it.',
+      //   );
+      // }
+
+      const deletedProperty = await this.propertiesRepo.findOneAndDelete({
+        _id: id,
+      });
+
+      return `property with name << ${deletedProperty.name}  >> has been deleted successfully`;
+    } catch (error) {
+      this.logger.error(`Failed to delete property with id ${id}`, error);
+
+      throw new InternalServerErrorException(error, error.message);
+    }
   }
 }
