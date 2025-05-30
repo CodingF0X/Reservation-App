afd30b6ca0bc3ff5d5e415de0e18c6ef67518dc4
CodingF0X <roody9a@gmail.com>
2025-05-30 12:17:28 +0200

    register the api gateway with grafana loki and Prometheus

--- api-gateway/apps/api-gateway/src/app.module.ts
+++ api-gateway/apps/api-gateway/src/app.module.ts
@@ -9,6 +9,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard, RolesGuard } from '@app/common/auth';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { HttpModule } from '@nestjs/axios';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
   imports: [
@@ -22,15 +23,15 @@ import { HttpModule } from '@nestjs/axios';
           transport: {
             targets: [
               // // send logs to Loki
-              // {
-              //   target: 'pino-loki',
-              //   options: {
-              //     host: configService.getOrThrow<string>('LOKI_URL'),
-              //     labels: { app: 'auth-service', env: 'development' },
-              //     batching: true,
-              //     interval: 5,
-              //   },
-              // },
+              {
+                target: 'pino-loki',
+                options: {
+                  host: configService.getOrThrow<string>('LOKI_URL'),
+                  labels: { app: 'auth-service', env: 'development' },
+                  batching: true,
+                  interval: 5,
+                },
+              },
               // // keeping pretty-printing for local development
               {
                 target: 'pino-pretty',
@@ -56,6 +57,18 @@ import { HttpModule } from '@nestjs/axios';
         }),
       },
     ]),
+
+     HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+
+    PrometheusModule.register({
+      path: '/metrics',
+      defaultMetrics: {
+        enabled: true,
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [
